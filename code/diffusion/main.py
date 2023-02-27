8d73e9ce409fe94663305058eea6234396c75831
thomas <tc@cerebriu.com>
2023-02-27 10:34:32 +0100

    Updated main-py to allow cla args

--- code/diffusion/main.py
+++ code/diffusion/main.py
@@ -6,20 +6,18 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s %(message)s',
                     level=logging.INFO,
                     datefmt="%I:%M:%S")
 
-
-def launch():
+def parse_args():
     parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    args.run_name = "DDPM_Unconditional"
-    args.epochs = 1
-    args.batch_size = 14
-    args.image_size = 64
-    args.num_classes = 10
-    args.dataset_path = "/Users/rasmus/Projects/Diffusion Models Framework/data/cifar10-64/train"
-    args.device = "cpu"
-    args.lr = 3e-4
-    train(args)
-
+    parser.add_argument("--run_name", type=str, default="DDPM_Unconditional")
+    parser.add_argument("--epochs", type=int, default=1000)
+    parser.add_argument("--batch_size", type=int, default=14)
+    parser.add_argument("--image_size", type=int, default=64)
+    parser.add_argument("--num_classes", type=int, default=10)
+    parser.add_argument("--dataset_path", type=str, default="/Users/rasmus/Projects/Diffusion Models Framework/data/cifar10-64/train")
+    parser.add_argument("--device", type=str, default="cpu")
+    parser.add_argument("--lr", type=float, default=3e-4)
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    launch()
+    args = parse_args()
+    train(args)
