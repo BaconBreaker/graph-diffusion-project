3adda46b4b916717fada66e7440cf2fb3e823012
thomas <tc@cerebriu.com>
2023-03-02 12:06:55 +0100

    Added metrics into util.py

--- code/diffusion/utils.py
+++ code/diffusion/utils.py
@@ -14,6 +14,8 @@ from diffpy.srreal.pdfcalculator import DebyePDFCalculator
 
 from mendeleev import element
 
+from scipy.stats import pearsonr
+
 
 def plot_images(images):
     plt.figure(figsize=(32, 32))
@@ -170,3 +172,52 @@ def calculate_pdf_batch(matrix_in, atom_species, pad_mask):
             pdfs.append(calculate_pdf(point_cloud, species))
 
     return torch.stack(pdfs)
+
+
+def pearson_metric(predicted_pdf, target_pdf):
+    """
+    Calculate pearson correlation coefficient between predicted and target pdf
+    args:
+        predicted_pdf (torch.Tensor): predicted pdf of shape (batch_size, 3000)
+        target_pdf (torch.Tensor): target pdf of shape (batch_size, 3000)
+    returns:
+        pearson (torch.Tensor): pearson correlation coefficient of shape (batch_size,)
+    """
+    pearson = []
+    for i in range(predicted_pdf.shape[0]):
+        pearson.append(
+            pearsonr(predicted_pdf[i].detach().cpu().numpy(), target_pdf[i].detach().cpu().numpy())[0]
+        )
+    return torch.tensor(pearson, dtype=torch.float32)
+
+
+def mse_metric(predicted_pdf, target_pdf):
+    """
+    Calculate mean squared error between predicted and target pdf
+    args:
+        predicted_pdf (torch.Tensor): predicted pdf of shape (batch_size, 3000)
+        target_pdf (torch.Tensor): target pdf of shape (batch_size, 3000)
+    returns:
+        mse (torch.Tensor): mean squared error of shape (batch_size,)
+    """
+    return torch.mean((predicted_pdf - target_pdf) ** 2, dim=1)
+
+
+def rwp_metric(predicted_pdf, true_pdf, sigmas=None):
+    """
+    Calculate the Reitveld Weighted Profile (RWP) metric
+    args:
+        predicted_pdf (torch.Tensor): predicted pdf of shape (batch_size, 3000)
+        target_pdf (torch.Tensor): target pdf of shape (batch_size, 3000)
+        sigmas (torch.Tensor): condfidence of predicted pdf, defaults to 1 (batch_size, 3000)
+    returns:
+        rwp (torch.Tensor): RWP metric of shape (batch_size,)
+    """
+    if sigmas is None:
+        sigmas = torch.ones_like(predicted_pdf)
+    sigmas_inv = 1 / sigmas
+    diff_squarred = (predicted_pdf - true_pdf) ** 2
+    true_squarred = true_pdf ** 2
+    rwp = torch.sqrt((sigmas_inv * diff_squarred).sum(dim=1) / (sigmas_inv * true_squarred).sum(dim=1))
+
+    return rwp
