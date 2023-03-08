a608ef1759f80420509e15feeb70fc0bb8362317
thomas <tc@cerebriu.com>
2023-03-08 13:35:25 +0100

    1

--- code/diffusion/train.py
+++ code/diffusion/train.py
@@ -4,16 +4,11 @@ Main training functionality for the diffusion model.
 @Author Thomas Christensen and Rasmus Pallisgaard
 """
 import pytorch_lightning as pl
-
 from dataloader import MoleculeDataModule
-
 from utils.get_config import get_model, get_diffusion
-from utils.logging import setup_logging
-
 from DiffusionWrapper import DiffusionWrapper
 
 def train(args):
-    setup_logging(args.run_name)
     model, pretransform, posttransform = get_model(args)
     diffusion_model = get_diffusion(args)
     dataloader = MoleculeDataModule(args=args, transform=pretransform)
