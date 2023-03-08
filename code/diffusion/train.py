c8563216a264df763e6dade2951f79865e91a977
Rasmus Pallisgaard <rasm796h@gmail.com>
2023-03-08 13:30:43 +0100

    added config setting to train.py

--- code/diffusion/train.py
+++ code/diffusion/train.py
@@ -7,7 +7,7 @@ import pytorch_lightning as pl
 
 from dataloader import MoleculeDataModule
 
-from utils.get_model import get_model
+from utils.get_config import get_model, get_diffusion
 from utils.logging import setup_logging
 
 from DiffusionWrapper import DiffusionWrapper
@@ -15,7 +15,7 @@ from DiffusionWrapper import DiffusionWrapper
 def train(args):
     setup_logging(args.run_name)
     model, pretransform, posttransform = get_model(args)
-    diffusion_model = get_diffusion()
+    diffusion_model = get_diffusion(args)
     dataloader = MoleculeDataModule(args=args, transform=pretransform)
     diffusion = DiffusionWrapper(denoising_fn=model, diffusion_model=diffusion_model, lr=args.lr, posttransform=posttransform)
 
