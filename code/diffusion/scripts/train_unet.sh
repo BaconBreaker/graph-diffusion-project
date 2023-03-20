dbc19ead6acdf2b2334721b591ea4a74b4a6c32e
Rasmus Pallisgaard <rasm796h@gmail.com>
2023-03-20 12:58:43 +0100

    small test update to slurm script

--- code/diffusion/scripts/train_unet.sh
+++ code/diffusion/scripts/train_unet.sh
@@ -12,9 +12,10 @@
 #SBATCH --time=8:00:00
 #Skipping many options! see man sbatch
 # From here on, we can start our program
-# python --version > echo
+python --version > echo
 echo "$CUDA_VISIBLE_DEVICES"
+nvcc --version > echo
 
 echo "Conditional UNET experiment:"
 
-python main.py --dataset_path /home/thomas/graph-diffusion-project/graphs_fixed_num_135/ --run_name 123 --max_epochs 1000 --check_val_every_n_epoch 10 --batch_size 4 --tensors_to_diffuse edge_sequence --pad_length 135 --diffusion_timesteps 3 --num_workers 8 --log_every_n_steps 10
+# python main.py --dataset_path /home/thomas/graph-diffusion-project/graphs_fixed_num_135/ --run_name 123 --max_epochs 1000 --check_val_every_n_epoch 10 --batch_size 4 --tensors_to_diffuse edge_sequence --pad_length 135 --diffusion_timesteps 3 --num_workers 8 --log_every_n_steps 10
