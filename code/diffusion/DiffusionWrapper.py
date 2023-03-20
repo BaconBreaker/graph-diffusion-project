5509aefc18f94f3a8668937d3b5c7a5f87c40222
thomas <tc@cerebriu.com>
2023-03-20 11:33:30 +0100

    1

--- code/diffusion/DiffusionWrapper.py
+++ code/diffusion/DiffusionWrapper.py
@@ -21,7 +21,7 @@ class DiffusionWrapper(pl.LightningModule):
     def forward(self, x):
         return self.denoising_fn(x)
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch, batch_idx, log_loss=True):
         batch_size = batch['pdf'].shape[0]
         t = self.diffusion_model.sample_time_steps(batch_size)
         noisy_batch, noise = self.diffusion_model.diffuse(batch, t)
@@ -39,18 +39,25 @@ class DiffusionWrapper(pl.LightningModule):
             for metric_name, metric_fn in self.metrics:
                 self.log(f"{i}: {metric_name}", metric_fn(pred, noise_i), prog_bar=True)
 
-        self.log("loss", loss)
+        if log_loss:
+            self.log("loss", loss)
 
         return loss
 
     def validation_step(self, batch, batch_idx):
+        # Get validation MSE on noise
+        val_loss = self.training_step(batch, batch_idx, log_loss=False)
+
+        # Generate samples and evaluate simulated pdfs
         samples = self.diffusion_model.sample(self.denoising_fn, batch)
         matrix_in, atom_species, r, pdf, pad_mask = self.posttransform(samples)
         predicted_pdf = calculate_pdf_batch(matrix_in, atom_species, pad_mask)
 
-        self.log("rwp", rwp_metric(predicted_pdf, pdf), prog_bar=True)
-        self.log("mse", mse_metric(predicted_pdf, pdf), prog_bar=True)
-        self.log("pearson", pearson_metric(predicted_pdf, pdf), prog_bar=True)
+        # Log metrics
+        self.log("val_loss", val_loss, prog_bar=True)
+        self.log("RWP", rwp_metric(predicted_pdf, pdf), prog_bar=True)
+        self.log("MSE of pdf", mse_metric(predicted_pdf, pdf), prog_bar=True)
+        self.log("Pearson", pearson_metric(predicted_pdf, pdf), prog_bar=True)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
