068516c4b9dbef71993b9f37dddcb6f4c75c33cc
thomas <tc@cerebriu.com>
2023-04-19 15:33:53 +0200

    1

--- code/diffusion/main.py
+++ code/diffusion/main.py
@@ -94,9 +94,9 @@ def parse_args():
                         help='Metrics during training, options are [mse, psnr, snr]')
 
     # ## Equivariant diffusion parameters ##
-    parser.add_argument("--equiv_n_layers", type=int, default=2,
+    parser.add_argument("--equiv_n_layers", type=int, default=5,
                         help="Number of layers in the equivariant diffusion model")
-    parser.add_argument("--equiv_hidden_dim", type=int, default=2,
+    parser.add_argument("--equiv_hidden_dim", type=int, default=10,
                         help="Hidden dimension of the equivariant diffusion model")
 
     # ## Conditional diffusion parameters ##
--- code/diffusion/scripts/train_eq_and_sample_complete.sh
+++ code/diffusion/scripts/train_eq_and_sample_complete.sh
@@ -24,8 +24,8 @@ echo "CUDA version:"
 # nvcc --version
 # 
 # --tensors_to_diffuse edge_sequence
-python main.py --dataset_path ../../graphs_h5/ --run_name ED_v3 \
-	--model self_attention --max_epochs 1000 --check_val_every_n_epoch 1 --batch_size 32 \
+python main.py --dataset_path ../../graphs_h5/ --run_name ED_v4 \
+	--model self_attention --max_epochs 10000 --check_val_every_n_epoch 1 --batch_size 64 \
 	--tensors_to_diffuse xyz_atom_species --pad_length 23 --diffusion_timesteps 1000 --num_workers 4 \
 	--log_every_n_steps 1 --device "cuda" --accelerator "gpu" --devices -1 --strategy "ddp" \
 	--disable_carbon_tracker --sample_interval 0 --enable_progress_bar False --single_sample --model equivariant
@@ -34,5 +34,5 @@ python ovito_complete_gif.py --dataset_path ../../graphs_h5/ --run_name eq_sampl
 	--model equivariant --batch_size 1 \
 	--tensors_to_diffuse xyz_atom_species --pad_length 23 --diffusion_timesteps 1000 \
     --device "cuda" --accelerator "gpu" --devices -1 --disable_carbon_tracker \
-	--t_skips 5 --checkpoint_path "checkpoints/ED_v3/last.ckpt" \
+	--t_skips 5 --checkpoint_path "checkpoints/ED_v4/last.ckpt" \
     --fix_noise --single_sample
