eddb24e29486c914c46bba63df031a8dd2c9904a
Rasmus Pallisgaard <rasm796h@gmail.com>
2023-04-22 20:15:52 +0200

    1

--- code/diffusion/scripts/train_eq_unconditional.sh
+++ code/diffusion/scripts/train_eq_unconditional.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #The partition is the queue you want to run on. standard is gpu and can be omitted.
-#SBATCH -p gpu --gres=gpu:titanrtx:2
-#SBATCH --job-name=graph_dif_selfatt
+#SBATCH -p gpu --gres=gpu:titanrtx:1
+#SBATCH --job-name=ED_uncond
 #number of independent tasks we are going to start in this script
 #number of cpus we want to allocate for each program
 #SBATCH --cpus-per-task=6
@@ -9,7 +9,7 @@
 #SBATCH --mem=16000M
 #We expect that our program should not run longer than 2 days
 #Note that a program will be killed once it exceeds this time!
-#SBATCH --time=24:00:00
+#SBATCH --time=4:00:00
 #Skipping many options! see man sbatch
 # From here on, we can start our program
 echo "Conditional UNET experiment:"
@@ -24,8 +24,9 @@ echo "CUDA version:"
 # nvcc --version
 # 
 # --tensors_to_diffuse edge_sequence
+#  --strategy "ddp"
 python main.py --dataset_path ../../graphs_fixed_num_135/ --run_name ED_uncond \
 	--model equivariant --max_epochs 10000 --check_val_every_n_epoch 1 --batch_size 64 \
 	--tensors_to_diffuse xyz_atom_species --pad_length 23 --diffusion_timesteps 1000 --num_workers 4 \
-	--log_every_n_steps 1 --device "cuda" --accelerator "gpu" --devices -1 --strategy "ddp" \
+	--log_every_n_steps 1 --device "cuda" --accelerator "gpu" --devices -1 \
 	--disable_carbon_tracker --sample_interval 0 --enable_progress_bar True --fast_dev_run
