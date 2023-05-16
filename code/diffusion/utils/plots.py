d6e7941c2440775b55e3307ba05dc516b0a260fc
Rasmus Pallisgaard <rasm796h@gmail.com>
2023-05-16 23:00:28 +0200

    1

--- code/diffusion/utils/plots.py
+++ code/diffusion/utils/plots.py
@@ -196,10 +196,8 @@ def make_histogram_single(positions, save_path, plot_positions=True):
     cmap.set_bad(cmap(0))
     h, xedges, yedges = np.histogram2d(x_fine, y_fine, bins=[400, 100])
     pcm = axes[1].pcolormesh(xedges, yedges, h.T, cmap=cmap,
-                             norm=LogNorm(vmin=0.0, vmax=1.5e2), rasterized=True)
-    print("pcm:")
-    print(pcm)
-    fig.colorbar(pcm, ax=axes[1], label="# points", pad=0)
+                             norm=LogNorm(vmax=1.5e2), rasterized=True)
+    # fig.colorbar(pcm, ax=axes[1], label="# points", pad=0)
     axes[1].set_title("2d histogram and log color scale")
     axes[1].set_xlabel("frames")
 
